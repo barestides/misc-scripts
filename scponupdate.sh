528d151f8b536019cc42db23bb0336b711998cd1
barestides <barestides@gmail.com>
2016-05-31 20:28:57 -0400

    jar file is now scpd to server upon update

--- scponupdate.sh
+++ scponupdate.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+while inotifywait -qqr "$1"; do 
+  echo "file updated";
+  scp -P 26 /home/barestides/IdeaProjects/ArcaneHelper/jar/CommandHelper.jar barestides@192.168.200.135:/home/barestides/server/plugins/CommandHelper.jar
+done
